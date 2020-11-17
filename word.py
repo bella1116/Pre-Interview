f48359182f27cb9fa495516326ffe2b3f05ceed7
YEHYUN <dldpgus7@gmail.com>
2020-11-18 02:45:13 +0900

    Hangman1 resource

--- word.py
+++ word.py
@@ -1,16 +1,27 @@
-import sys
+import random
 
-fname = input("Enter data file name : ")
-try:
-    fH = open(fname)
-except FileNotFoundError as e:
-    print("No such file : " + fname)
-    sys.exit()
-wordcount = {}
-for line in fH:
-    words = line.split()
-    for word in words:
-        wordcount[word] = wordcount.get(word, 0) + 1
-fH.close()
-for word in wordcount:
-    print(word, ":", wordcount[word])
+class Word:
+
+    def __init__(self, filename):
+        self.words = []
+        f = open(filename, 'r')
+        lines = f.readlines()
+        f.close()
+
+        self.count = 0
+        for line in lines:
+            word = line.rstrip()
+            self.words.append(word)
+            self.count += 1
+
+        print('%d words in DB' % self.count)
+
+
+    def test(self):
+        return 'default'
+
+
+    def randFromDB(self):
+        r = random.randrange(self.count)
+        return self.words[r]
+
